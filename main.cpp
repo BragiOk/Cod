5c695a9f8357af6f3a27eb339517c9a3531e935a
BragiOk <raven8007@gmail.com>
2017-01-22 13:45:34 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a;
+    cin >> a;
+    cout << a << endl;
+    cin >> a;
+    cout << a << endl;
+    cin.get();
+    return 0;
+
+}
